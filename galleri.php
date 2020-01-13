db7a17b6976b9c2bcb3c2757d5e42893753488d6
gohovd <goran.hovde@gmail.com>
2020-01-13 16:37:29 +0100

    kontakt og galleri

--- galleri.php
+++ galleri.php
@@ -12,18 +12,90 @@ include('./nav-bar.php');
 ?>
 
 <body>
-	<div class="under-construction">
-		<p>Under konstruksjon</p>
-		<a href="<?php echo $baseUrl ?>"><i class="fa fa-arrow-left" aria-hidden="true"></i></a>
+
+	<div id="slideshowHolder" class="container"></div>
+
 	</div>
+
+	<script>
+		const DIR = './media/foer/';
+		const IMAGE_NAME = 'foer';
+		const FILE_EXTENSION = '.jpg';
+		const IMAGE_COUNT = 22;
+
+		const captions = [{
+			"id": "1",
+			"text": "<i>Foto</i>: <i class='fa fa-facebook-official' aria-hidden='true'></i>&nbsp;<a href='https://www.facebook.com/molnesdronetjenester/'>Molnes Dronetjenester</a>"
+		}];
+
+		function checkImage(imageSrc, good, bad) {
+			var img = new Image();
+			img.onload = good;
+			img.onerror = bad;
+			img.src = imageSrc;
+		}
+
+		for (var i = 1; i <= IMAGE_COUNT; i++) { // increase or decrease i as you add or remove images from foer folder
+			var image_path = DIR + IMAGE_NAME + i + FILE_EXTENSION;
+			
+			var imgDiv = document.createElement('div'); // div container for img
+			var img = document.createElement('img'); // img
+
+			img.src = DIR + IMAGE_NAME + i + FILE_EXTENSION; // graphics/foer/[number].jpg
+			img.className = 'img-fluid'; // add classes to the img element
+
+			imgDiv.id = 'imgDiv' + i; // give the img container an id (div)
+			imgDiv.className = 'imgDiv'; // add classes to the div element
+
+			var caption = document.createElement('div');
+			caption.id = 'caption' + i;
+			caption.className = 'caption';
+
+			document.getElementById('slideshowHolder').appendChild(imgDiv); // add the container to the slideshow holder
+			document.getElementById('imgDiv' + i).appendChild(img); // add the image to its container
+			document.getElementById('imgDiv' + i).appendChild(caption);
+		}
+
+		// Attach all the captions to their imgDivs
+		for (i in captions) {
+			var id = captions[i].id;
+			var c = document.getElementById('caption' + id);
+			c.innerHTML = captions[i].text;
+		}
+	</script>
+
+	<button class="btn btn-secondary float-left" onclick="plusDivs(-1)">&#10094;</button>
+	<button class="btn btn-secondary float-right" onclick="plusDivs(1)">&#10095;</button>
+
+	</div>
+
+	<script>
+		var slideIndex = 1; // initial index (i.e. foer/1 is first image to be shown)
+
+		showDivs(slideIndex);
+
+		function plusDivs(n) {
+			showDivs(slideIndex += n);
+		}
+
+		function showDivs(n) {
+			var i;
+			var x = document.getElementsByClassName("imgDiv");
+			if (n > x.length) {
+				slideIndex = 1
+			} // if the slideIndex goes over max images, rollover to 1
+			if (n < 1) {
+				slideIndex = x.length
+			}
+			for (i = 0; i < x.length; i++) { // hide all images
+				x[i].style.display = "none";
+			}
+			x[slideIndex - 1].style.display = "block"; // ... except one
+		}
+	</script>
+
 </body>
 
-<style>
-	body {
-		background-color: rgba(68, 85, 140, 0.8);
-	}
-</style>
-
 <?php
-	include('./foot.php');
+include('./foot.php');
 ?>
--- kontakt.php
+++ kontakt.php
@@ -1,6 +1,6 @@
 <?php
 if (session_id() == "") {
-    session_start();
+	session_start();
 }
 ?>
 <?php $title = 'Kontakt'; ?>
@@ -10,18 +10,83 @@ if (session_id() == "") {
 <?php include('./nav-bar.php'); ?>
 
 <body>
-	<div class="under-construction">
-		<p>Under konstruksjon</p>
-		<a href="<?php echo $baseUrl ?>"><i class="fa fa-arrow-left" aria-hidden="true"></i></a>
+
+	<div class="container" style="margin-top: 25px;">
+
+		<div class="row">
+
+			<div class="col-md-4">
+				<div class="card">
+					<div class="card-header" style="color: black;">
+						<i class="fa fa-envelope" style="color: rgb(68, 85, 140); font-size: 1.3em;"></i>&nbsp;&nbsp;<i>Brevpost sendes til</i>
+					</div>
+					<div class="card-body">
+						<h5 class="card-title" style="color: black;">Addresse</h5>
+						<p class="card-text">
+							Vike Småbåtforening ved Bjørn Ove Ulla<br>
+							Heen-feltet<br>
+							6392 Vikebukt</p>
+					</div>
+				</div>
+			</div>
+
+
+			<style>
+				table th {
+					color: black;
+				}
+			</style>
+			<div class="col-md-8">
+				<table class="table">
+					<tr>
+						<th></th>
+						<th>Navn</th>
+						<th>Epost</th>
+						<th>Telefon<br></th>
+					</tr>
+					<tr>
+						<td>Formann</td>
+						<td>Bjørn Ove Ulla<br></td>
+						<td>bjorn.ulla (at) timpex.no</td>
+						<td>924 34 571<br></td>
+					</tr>
+					<tr>
+						<td>Nest formann<br></td>
+						<td>Ola Vik<br></td>
+						<td>ola.vik (at) nasta.no<br></td>
+						<td>977 53 093<br></td>
+					</tr>
+					<tr>
+						<td>Kasserer</td>
+						<td>Sveinung Dragnes<br></td>
+						<td>sveinung.dragnes (at) online.no</td>
+						<td>938 44 396<br></td>
+					</tr>
+					<tr>
+						<td>Referent</td>
+						<td>Jan Olav Forland<br></td>
+						<td>jan.olav.forland (at) mibox.no</td>
+						<td>903 53 720<br></td>
+					</tr>
+					<tr>
+						<td>Styremedlem</td>
+						<td>Odd Johnny Villa<br></td>
+						<td>oj.villa (at) hotmail.com</td>
+						<td>917 80 424<br></td>
+					</tr>
+				</table>
+			</div>
+
+		</div>
+
+		<div class="row" id="contact-form">
+			<div class="col-lg-12">
+				
+			</div>
+		</div>
 	</div>
 </body>
 
-<style>
-	body {
-		background-color: rgba(68, 85, 140, 0.8);
-	}
-</style>
-
 <?php
-	include('./foot.php');
+include('./foot.php');
 ?>
